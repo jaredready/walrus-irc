9d451932581beba3facdc1f91877c35a4f8f6dd1
ResidentBiscuit <jared.ready1@gmail.com>
2015-02-12 15:34:45 -0600

    A lot of interface work

--- javascript/walrus.js
+++ javascript/walrus.js
@@ -45,15 +45,117 @@ client.addListener('message', function(nick, to, text) {
 });
 
 client.addListener('names', function(channel, nicks){
-	var channel_panel = document.getElementById('botdever-panel');
+	log.info(channel);
+	var channel_panel = document.getElementById('channel0-server0-panel');
 	var channel_user_list = (channel_panel.getElementsByClassName('channel-user-list'))[0];
+	log.info(nicks);
 	Object.keys(nicks).forEach(function(nick) {
-		log.info(nick);
-		var user = document.createElement("li");
-		user.appendChild(document.createTextNode(nick));
+		var user = document.createElement('a');
+		user.classList.add('list-group-item');
+		user.href = '#';
+
+		if(nicks[nick] === '@') {
+			var glyphiconSpan = document.createElement('span');
+			glyphiconSpan.classList.add('glyphicon', 'glyphicon-exclamation-sign');
+		}
+		else {
+			var glyphiconSpan = document.createElement('span');
+			glyphiconSpan.classList.add('glyphicon', 'glyphicon-user');
+		}
+
+		user.appendChild(glyphiconSpan);
+		user.appendChild(document.createTextNode(" " + nick));
 		channel_user_list.appendChild(user);
 	});
 });
+
+client.addListener('join', function(channel, nick, message){
+	var message_table = document.getElementById('messageTable');
+	var message_row = message_table.insertRow(-1);
+	var timestamp = moment();
+
+	var timestamp_cell = message_row.insertCell(0);
+	timestamp_cell.classList.add('message-timestamp');
+	timestamp_cell.innerHTML = '1/1/2015 13:31:55';
+
+	var from_cell = message_row.insertCell(1);
+	from_cell.classList.add('message-nick');
+	var join_icon = document.createElement('span');
+	join_icon.classList.add('glyphicon', 'glyphicon-arrow-right');
+	from_cell.appendChild(join_icon);
+
+	var message_cell = message_row.insertCell(2);
+	message_cell.classList.add('message-text');
+	message_cell.innerHTML = nick + ' has joined';
+});
+
+client.addListener('part', function(channel, nick, reason, message){
+	var message_table = document.getElementById('messageTable');
+	var message_row = message_table.insertRow(-1);
+	var timestamp = moment();
+
+	var timestamp_cell = message_row.insertCell(0);
+	timestamp_cell.classList.add('message-timestamp');
+	timestamp_cell.innerHTML = '1/1/2015 13:31:55';
+
+	var from_cell = message_row.insertCell(1);
+	from_cell.classList.add('message-nick');
+	var join_icon = document.createElement('span');
+	join_icon.classList.add('glyphicon', 'glyphicon-arrow-left');
+	from_cell.appendChild(join_icon);
+
+	var message_cell = message_row.insertCell(2);
+	message_cell.classList.add('message-text');
+	message_cell.innerHTML = nick + ' has left. (' + reason + ')';
+});
+
+document.getElementById('message-button').addEventListener('click', function(){
+	var message = document.getElementById('messageInput');
+	client.say('#botdever', message.value);
+
+	var message_table = document.getElementById('messageTable');
+	var message_row = message_table.insertRow(-1);
+	var timestamp = moment();
+
+	var timestamp_cell = message_row.insertCell(0);
+	timestamp_cell.classList.add('message-timestamp');
+	timestamp_cell.innerHTML = '1/1/2015 13:31:55';
+
+	var from_cell = message_row.insertCell(1);
+	from_cell.classList.add('message-nick');
+	from_cell.innerHTML = clientConfig.userName;
+
+	var message_cell = message_row.insertCell(2);
+	message_cell.classList.add('message-text');
+	message_cell.innerHTML = message.value;
+
+	message.value = "";
+});
+
+document.getElementById('messageForm').addEventListener('submit', function(ev){
+	var message = document.getElementById('messageInput');
+	client.say('#botdever', message.value);
+
+	var message_table = document.getElementById('messageTable');
+	var message_row = message_table.insertRow(-1);
+	var timestamp = moment();
+
+	var timestamp_cell = message_row.insertCell(0);
+	timestamp_cell.classList.add('message-timestamp');
+	timestamp_cell.innerHTML = '1/1/2015 13:31:55';
+
+	var from_cell = message_row.insertCell(1);
+	from_cell.classList.add('message-nick');
+	from_cell.innerHTML = clientConfig.userName;
+
+	var message_cell = message_row.insertCell(2);
+	message_cell.classList.add('message-text');
+	message_cell.innerHTML = message.value;
+
+	message.value = "";
+
+	ev.preventDefault();
+})
 
 client.addListener('raw', function(message) {
 	log.info(message);
