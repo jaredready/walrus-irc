0641149e98e0c5bd8c8148902eb3d050d1bf0b05
ResidentBiscuit <jared.ready1@gmail.com>
2015-02-19 13:11:01 -0600

    Database

---
+++ javascript/db.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/walrus');
+
+var Message = new mongoose.Schema({
+	nick: String,
+	channel: String,
+	message: String,
+	time: Number
+});
+
+var Channel = new mongoose.Schema({
+	server: String,
+	channel: String
+});
+
+var User = new mongoose.Schema({
+	server: String,
+	channel: String,
+	nick: String
+})
+
+var entry = {
+	message: mongoose.model('Message', Message),
+	channel: mongoose.model('Channel', Channel),
+	user: mongoose.model('User', User)
+};
+
+module.exports = entry;
