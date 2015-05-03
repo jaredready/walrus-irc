ef6a3759befb3a330fd2c642322b62c1db7f7b1c
ResidentBiscuit <jared.ready1@gmail.com>
2015-05-03 18:12:00 -0500

    Initial menu work

---
+++ js/menus.js
@@ -0,0 +1,53 @@
+var gui = require('nw.gui');
+
+var menu_bar = new gui.Menu({ type: 'menubar' });
+
+var file = new gui.MenuItem({ label: 'File' });
+var edit = new gui.MenuItem({ label: 'Edit' });
+var help = new gui.MenuItem({ label: 'Help' });
+
+// File submenus
+var file_submenu = new gui.Menu();
+file_submenu.append(new gui.MenuItem({
+	label: 'Quit',
+	click: function() {
+		gui.Window.get().close();
+	}
+}));
+file_submenu.append(new gui.MenuItem({
+	label: 'Lock'
+}));
+file.submenu = file_submenu;
+
+// Help submenus
+var help_submenu = new gui.Menu();
+help_submenu.append(new gui.MenuItem({
+	label: 'Show Dev Tools',
+	click: function() {
+		if(!gui.Window.get().isDevToolsOpen()) {
+			gui.Window.get().showDevTools();
+		}
+		else {
+			gui.Window.get().closeDevTools();
+		}
+	}
+}));
+help_submenu.append(new gui.MenuItem({ label: 'About' }));
+help.submenu = help_submenu;
+
+// Connect menu items to menubar
+menu_bar.append(file);
+menu_bar.append(edit);
+menu_bar.append(help);
+
+gui.Window.get().menu = menu_bar;
+
+/**
+ *	Event listeners - Need to move to a new file sometime...
+ */
+
+gui.Window.get().on('close', function() {
+	this.hide();
+	console.log("Cleaning up...");
+	this.close(true);
+});
